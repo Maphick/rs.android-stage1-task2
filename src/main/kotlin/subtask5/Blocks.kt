494f0ff675e3338358a870525e2229431f75a061
Makashova Mariia <maphick@yandex.ru>
2020-04-11 13:14:08 +0300

    feat: try to commit from intellij idea

--- src/main/kotlin/subtask5/Blocks.kt
+++ src/main/kotlin/subtask5/Blocks.kt
@@ -5,7 +5,16 @@ import kotlin.reflect.KClass
 class Blocks {
 
     // TODO: Complete the following function
-    fun getData(blockA: Array<Any>, blockB: KClass<*>): Any {
+    fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
+        val properties = System.getProperties()
+
         throw NotImplementedError("Not implemented")
     }
+}
+
+fun main() {
+    val blockA = arrayOf(1, "4", 2, "3")
+    var result = robotPaphnutiy.getData(blockA, String::class)
+    print(result)
+    //assertEquals("43", robotPaphnutiy.getData(blockA, String::class))
 }
